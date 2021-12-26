98ae9c3ab01ca30a5d34e9f4caf8f01c4c0cfaf4
jacobsonryan <ryanhjacobson@gmail.com>
2021-12-25 23:27:49 -0500

    png to jpg

--- src/App.js
+++ src/App.js
@@ -6,13 +6,14 @@ function App() {
 	let  [bombCounter, setBombCounter] = useState(0)
 	const [board, setBoard] = useState(initBoard())
 	const [gameOver, setGameOver] = useState(false)
+	const [status, setStatus] = useState(0)
  
 	function initBoard() {
 		let tempBoard = Array.from(Array(16), () => new Array(16))
 
 		for(let i = 0; i < tempBoard.length; i++) {
 			for(let j = 0; j < tempBoard[i].length; j++) {
-				tempBoard[i][j] = {count: 0, visible: false, flagged: false, src: '0.png'}
+				tempBoard[i][j] = {count: 0, visible: false, flagged: false, src: '0.jpg'}
 			}
 		}
 
@@ -20,7 +21,7 @@ function App() {
 			let randomX = Math.floor(Math.random() * tempBoard.length)
 			let randomY = Math.floor(Math.random() * tempBoard.length)
 			tempBoard[randomX][randomY].count = -1
-			tempBoard[randomX][randomY].src = 'bomb.png'
+			tempBoard[randomX][randomY].src = 'bomb.jpg'
 			bombCounter++
 		}
 
@@ -33,7 +34,7 @@ function App() {
 							if(tempBoard[i + xOffset]?.[j + yOffset].count === -1 && tempBoard[i][j].count !== -1) {
 								counter++
 								tempBoard[i][j].count = counter
-								tempBoard[i][j].src = counter + '.png'
+								tempBoard[i][j].src = counter + '.jpg'
 							}
 						}
 					}
@@ -78,16 +79,17 @@ function App() {
 		let tempBoard = [...board]
 		if(tempBoard[rIndex][cIndex].count === -1 && !tempBoard[rIndex][cIndex].flagged) {
 			setGameOver(true)
+			setStatus(1)
 			for(let i = 0; i < tempBoard.length; i++) {
 				for(let j = 0; j < tempBoard[i].length; j++) {
 					if(tempBoard[i][j].count === -1 && !tempBoard[i][j].visible) {
-						tempBoard[i][j].src = 'shownBomb.png'
+						tempBoard[i][j].src = 'shownBomb.jpg'
 						tempBoard[i][j].visible = true
 					}
 					if(tempBoard[i][j].count !== -1 && tempBoard[i][j].flagged) {
 						tempBoard[i][j].flagged = false
 						tempBoard[i][j].visible = true
-						tempBoard[i][j].src = 'wrongBomb.png'
+						tempBoard[i][j].src = 'wrongBomb.jpg'
 					}
 				}
 			}
@@ -116,6 +118,7 @@ function App() {
 					setBoard(tempBoard)
 				}
 			}
+			setStatus(2)
 			setGameOver(true)
 		}
 	}
@@ -126,12 +129,20 @@ function App() {
 
 	return (
 		<>
-		<div className="board">
+
+		<button onClick={(() => {
+			setGameOver(false)
+			setStatus(0)
+			setBoard(initBoard())
+		})}>Reset Board</button>
+		<p>{status === 0 ? '' : '' || status === 1 ? 'You Lost!' : '' || status === 2 ? 'You Win!': ''}</p>
+		<div className="board"> 
 		{board.map((row, rIndex) => {
 			return(row.map((cell, cIndex) => {
 				return(
 					<img id="cell" key={cIndex} onClick={(() => {
 						if(!gameOver) {
+							setStatus(0)
 							show(rIndex, cIndex)
 							checkLose(rIndex, cIndex)
 						}
@@ -142,8 +153,8 @@ function App() {
 						}
 					}
 					src={
-						cell.flagged ? require('./flag.png') : '' ||
-						!cell.visible ? require('./empty.png') : '' ||
+						cell.flagged ? require('./flag.jpg') : '' ||
+						!cell.visible ? require('./empty.jpg') : '' ||
 						require('./' + cell.src)
 					} 
 					/> 
